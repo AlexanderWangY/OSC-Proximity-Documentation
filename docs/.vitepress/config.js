713c730f50b03c14a3ed3a63595195fa725244fc
h1divp <71522316+h1divp@users.noreply.github.com>
2024-02-15 16:55:51 -0500

    Readded Firestore Collections page

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -31,6 +31,7 @@ const documentation = [
       { text: "Introduction", link: "/guide/backend-introduction.md" },
       { text: "REST API", link: "/guide/backend-restapi.md" },
       { text: "Websocket API", link: "/guide/backend-websockets.md" },
+      { text: 'Firestore Collections & Types', link: '/guide/backend-types.md'}
     ],
   },
 ];
